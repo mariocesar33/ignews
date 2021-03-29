f2886840941afc10d16fbdb028b8a5182dc3a546
Mário César Silva <mcgato33@hotmail.com>
2021-03-29 21:10:20 -0100

    criar _document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import Head from 'next/head';
+
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Hello word</h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+
+        <meta name="description" content=""/>
+      </Head>
+      <h1 className={styles.title}>
+        Hello <span>word</span>
+      </h1>
+    </>
   );
 }
