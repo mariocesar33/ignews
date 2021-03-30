f3b7ba2cc4df3c77bfb9e28b5e67ae2678c36565
Mário César Silva <mcgato33@hotmail.com>
2021-03-30 13:56:36 -0100

    Pagina Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import styles from '../styles/pages/home.module.scss';
+
 export default function Home() {
   return (
     <>
@@ -8,9 +10,14 @@ export default function Home() {
         <meta name="description" content=""/>
       </Head>
 
-      <main>
-        <section>
-          <span> Hey, welcome</span>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get access to all the publications<br/>
+            <span>for $9.90 month</span>
+          </p>
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding"/>
