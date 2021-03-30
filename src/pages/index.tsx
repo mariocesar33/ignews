5ad28b11549d7271edb430d0d8ddaef0178d1925
Mário César Silva <mcgato33@hotmail.com>
2021-03-30 11:05:54 -0100

    Componente: SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,17 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
         <meta name="description" content=""/>
       </Head>
+
+      <main>
+        <section>
+          <span> Hey, welcome</span>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding"/>
+      </main>
     </>
   );
 }
