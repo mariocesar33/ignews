687ad7fd1b20d3f6352bbd9c18b4c86d620c25b3
Mário César Silva <mcgato33@hotmail.com>
2021-03-31 13:12:06 -0100

    Parametrização nas rotas

---
+++ src/pages/api/users/[...params].tsx
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  console.log(request.query);
+
+  const users = [
+    { id: 1, name: 'Mário César' },
+    { id: 2, name: 'Ailton Fortes' },
+    { id: 3, name: 'Irka Carvalho' }
+  ];
+
+  return response.json(users);
+}
